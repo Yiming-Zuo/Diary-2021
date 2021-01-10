54414ed7ab5bce2a6bab081f6d1f142cd834009b
Yiming-Zuo <zuoyiming@stu.ouc.edu.cn>
2021-01-10 22:42:35 +0800

    2021.1.10

---
+++ 35.搜索插入位置.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=cpp
+ *
+ * [35] 搜索插入位置
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + ()
+        }
+    }
+};
+// @lc code=end
+
