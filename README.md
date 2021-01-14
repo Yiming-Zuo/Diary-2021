682304f60a26d54555f19ad0acb12bdd1a623ea6
Yiming-Zuo <zuoyiming@stu.ouc.edu.cn>
2021-01-14 23:15:27 +0800

    01.14

--- README.md
+++ README.md
@@ -58,4 +58,5 @@
 * 1.13
     * 字符串
         * 双指针[344.反转字符串](https://github.com/Yiming-Zuo/Diary-2021/blob/main/leetcode/344.反转字符串.cpp)
-        * [541.反转字符串2](https://github.com/Yiming-Zuo/Diary-2021/blob/main/leetcode/541.反转字符串2.cpp)
+        * 固定规律一段一段处理字符串时使用for循环[541.反转字符串2](https://github.com/Yiming-Zuo/Diary-2021/blob/main/leetcode/541.反转字符串2.cpp)
+        * 双指针 填充类问题 -> 先扩容，再从后向前填充[剑指05.替换空格](https://github.com/Yiming-Zuo/Diary-2021/blob/main/leetcode/剑指05.替换空格.cpp)
--- leetcode/剑指05.替换空格.cpp
+++ leetcode/剑指05.替换空格.cpp
@@ -1,14 +1,10 @@
-# 扩充字符串，原地修改
-
-* 统计原字符串中空格数量，由此来确定扩充字符串的大小。
-* 倒序遍历修改扩充后字符串。
-
-时间复杂度O(N): 遍历统计空格数量、遍历修改字符串
-空间复杂度O(1)
-
-```cpp
 class Solution {
 public:
+    // 双指针原地修改
+    // 统计原字符串中空格数量，由此来确定扩充字符串的大小。
+    // 倒序遍历修改扩充后字符串。
+    // 时间复杂度O(N): 遍历统计空格数量、遍历修改字符串
+    // 空间复杂度O(1)
     string replaceSpace(string s) {
         int count = 0, len = s.size();
         for (auto c : s) {
@@ -26,17 +22,9 @@ public:
         }
         return s;
     }
-};
-```
-
-# String 内置算法
-
-* `int find(const char c)  // 查找不到返回-1`
-* `string& replace(int pos, int n, const string str)  // 替换从pos开始n个字符为字符串str`
-
-```cpp
-class Solution {
-public:
+    // String 内置算法
+    // int find(const char c)
+    // string& replace(int pos, int n, const string str)  替换从pos开始n个字符为字符串str
     string replaceSpace(string s) {
         int pos = s.find(' ');
         while (pos != -1) {
@@ -45,17 +33,9 @@ public:
         }
         return s;
     }
-};
-```
-
-# 开辟新数组
-
-时间复杂度O(N)
-空间复杂度O(N)
-
-```cpp
-class Solution {
-public:
+    // 开辟新数组
+    // 时间复杂度O(N)
+    // 空间复杂度O(N)
     string replaceSpace(string s) {
         string new_s;
         for (auto c : s) {
@@ -65,4 +45,3 @@ public:
         return new_s;
     }
 };
-```
