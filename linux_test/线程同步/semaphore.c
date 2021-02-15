e985771513e516d9c8db14cae3f0da061f860703
Yiming-Zuo <zuoyiming@stu.ouc.edu.cn>
2021-02-15 13:30:39 +0800

    20210215

--- linux_test/线程同步/semaphore.c
+++ linux_test/线程同步/semaphore.c
@@ -27,6 +27,7 @@ void *producer(void *arg) {
         printf("ptid %lu 生产结点 %d\n", pthread_self(), head->val);
 
         pthread_mutex_unlock(&mtx);
+        // 通知消费者
         sem_post(&csem);  // csem++  csem <= 6
     }
 }
@@ -34,23 +35,26 @@ void *producer(void *arg) {
 void *customer(void *arg) {
     int count = 10;
     while (count--) {
+        int csem_val = sem_getvalue(&csem, &csem_val);
+        printf("csem: %d\n", csem_val);
         sem_wait(&csem);  // csem--
+        // 如果有多个线程进入，串行运行
         pthread_mutex_lock(&mtx);
-        
+
         struct ListNode *tmp = head;
         head = head->next;
         printf("ctid %lu 消费结点 %d\n", pthread_self(), tmp->val);
         free(tmp);
 
         pthread_mutex_unlock(&mtx);
-        sem_post(&psem);  // psem <= 6  放在互斥后解锁以后
+        sem_post(&psem);  // psem <= 8  放在互斥后解锁以后
     }
 }
 
 int main() {
     pthread_t ptid[5], ctid[5];
     pthread_mutex_init(&mtx, NULL);
-    sem_init(&psem, 0, 6);
+    sem_init(&psem, 0, 8);
     sem_init(&csem, 0, 0);  // 消费者默认阻塞
 
     for (int i = 0; i < 5; i++) {
